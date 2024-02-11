efd02d04a7fb40aeef6ef017689845b09acab7f5
Benjamin-Goodluck <148908812+Benjamin-Goodluck@users.noreply.github.com>
2024-02-11 17:10:22 +0100

    Add files via upload

---
+++ g portfolio/script.js
@@ -0,0 +1,23 @@
+function sendToWhatsapp(){
+	let number = "+2347064078742";
+
+	let name = document.getElementById('name').value;
+	let email = document.getElementById('email').value;
+	let message = document.getElementById('message').value;
+
+	var url = "https://wa.me/" + number + "?text="
+	+ "Name : " +name+ "%0a"
+	+ "Email : " +email+ "%0a"
+	+ "Message : " +message+ "%0a%0a";
+
+	window.open(url, '_blank').focus();
+}
+
+// Shuffle projects every 10 seconds
+setInterval(() => {
+    const projectsContainer = document.querySelector('.projects');
+    const projects = Array.from(document.querySelectorAll('.project'));
+    projects.forEach((project, index) => {
+        projectsContainer.appendChild(project);
+    });
+}, 10000);
